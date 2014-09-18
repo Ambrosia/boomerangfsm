6584f14719ac2f1c878358cdab3032afba8c5ba3
Ambrosia <ambrosia@hana.re>
2014-09-18 02:17:25 +0100

    Improve Player test
    Rewrote what little I had.

--- test/player_test.exs
+++ test/player_test.exs
@@ -1,15 +1,32 @@
 defmodule BoomerangFSM.PlayerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   alias BoomerangFSM.Player
 
-  test "two players can be created and be assigned to each other as opponents" do
+  test "start_link arguments work correctly" do
     assert {:ok, player1} = Player.start_link name: "Player 1"
     assert {:ok, player2} = Player.start_link name: "Player 2", opponent: player1
 
-    Player.add_opponent player1, player2
+    assert {:awaiting_opponent, %Player{name: "Player 1"}} = Player.state player1
+    assert {:ready_to_play, %Player{opponent: ^player1}} = Player.state player2
+  end
 
-    assert {:ready_to_throw, %Player{opponent: player2}} = Player.state player1
-    assert {:ready_to_throw, %Player{opponent: player1}} = Player.state player2
+  test "players can be assigned to each other" do
+    {player1, player2} = create_two_players |> assign_players_to_each_other
+
+    assert {_, %Player{opponent: ^player2}} = Player.state player1
+    assert {_, %Player{opponent: ^player1}} = Player.state player2
+  end
+
+  defp create_two_players do
+    {:ok, player1} = Player.start_link name: "Player 1"
+    {:ok, player2} = Player.start_link name: "Player 2"
+    {player1, player2}
+  end
+
+  defp assign_players_to_each_other({player1, player2}) do
+    Player.add_opponent player1, player2
+    Player.add_opponent player2, player1
+    {player1, player2}
   end
 end
