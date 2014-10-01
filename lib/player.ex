5d6e98f5509026b8b0cdadf4b5fc782d4efd637d
Ambrosia <ambrosia@hana.re>
2014-10-01 04:37:17 +0100

    Player: implemented boomerang throwing
    - Boomerangs have a flight time
    - Upon throwing, the player will tell their opponent to catch (so they
      become ready to catch), then they will become ready to catch
      themselves.
    - The player can catch the boomerang after they are ready for it
    - There is a 33% chance of a players opponent catching the boomerang,
      a 34% chance of the boomerang returning back to the player and a 33%
      chance of it being lost.
    - Added some test functions (I'm iffy about this)

--- lib/player.ex
+++ lib/player.ex
@@ -1,10 +1,17 @@
 defmodule BoomerangFSM.Player do
   @wait_to_catch_time 6000
+  @boomerang_flight_time 1000
 
   @behaviour :gen_fsm
 
   @derive [Access]
-  defstruct name: nil, opponent: nil, wait_to_catch_time: 6000
+  defstruct [
+    name: nil,
+    opponent: nil,
+    wait_to_catch_time: @wait_to_catch_time,
+    boomerang_flight_time: @boomerang_flight_time
+  ]
+
   alias __MODULE__
 
   # Public
@@ -18,6 +25,35 @@ defmodule BoomerangFSM.Player do
 
   def give_boomerang(pid), do: :gen_fsm.send_event(pid, :give_boomerang)
 
+  def throw_boomerang(pid, test: :boomerang_lost), do: :gen_fsm.send_event(pid, :throw_boomerang)
+  def throw_boomerang(pid, test: :boomerang_return) do
+    :gen_fsm.send_event(pid, :throw_boomerang)
+    {_, state} = Player.state pid
+    :timer.sleep state.boomerang_flight_time
+    :gen_fsm.send_event(pid, :caught_boomerang)
+  end
+  def throw_boomerang(pid, test: :boomerang_opponent) do
+    :gen_fsm.send_event(pid, :throw_boomerang)
+    {_, state} = Player.state pid
+    :timer.sleep state.boomerang_flight_time
+    :gen_fsm.send_event(state.opponent, :caught_boomerang)
+  end
+
+  def throw_boomerang(pid) do
+    :gen_fsm.send_event(pid, :throw_boomerang)
+
+    {_, state} = Player.state pid
+    :timer.sleep @boomerang_flight_time
+    case :random.uniform do
+      # other player catches boomerang
+      num when num < 0.33 -> :gen_fsm.send_event(state.opponent, :caught_boomerang)
+      # boomerang returns to this player
+      num when num < 0.67 -> :gen_fsm.send_event(pid, :caught_boomerang)
+      # boomerang was dropped, lost or shot by a ufo
+      _ -> :ok
+    end
+  end
+
   # State machine
   def awaiting_opponent({:add_opponent, opponent}, state) do
     state = %{state | opponent: opponent}
@@ -29,6 +65,17 @@ defmodule BoomerangFSM.Player do
   end
 
   def ready(:give_boomerang, state), do: {:next_state, :ready_to_throw, state}
+  def ready(:catch!, state) do
+    {:next_state, :ready_to_catch, state, state.wait_to_catch_time}
+  end
+
+  def ready_to_throw(:throw_boomerang, state) do
+    :gen_fsm.send_event(state.opponent, :catch!)
+    {:next_state, :ready_to_catch, state, state.wait_to_catch_time}
+  end
+
+  def ready_to_catch(:caught_boomerang, state), do: {:next_state, :ready_to_throw, state}
+  def ready_to_catch(:timeout, state), do: {:next_state, :ready, state}
 
   # OTP stuff
   def start_link(args \\ []) do
@@ -36,10 +83,13 @@ defmodule BoomerangFSM.Player do
   end
 
   def init(args) do
+    :random.seed :erlang.now
+
     state = %Player{
       name: Keyword.get(args, :name),
       opponent: Keyword.get(args, :opponent),
-      wait_to_catch_time: Keyword.get(args, :wait_to_catch_time, @wait_to_catch_time)
+      wait_to_catch_time: Keyword.get(args, :wait_to_catch_time, @wait_to_catch_time),
+      boomerang_flight_time: Keyword.get(args, :boomerang_flight_time, @boomerang_flight_time)
     }
 
     case state.opponent do
--- test/player_test.exs
+++ test/player_test.exs
@@ -4,6 +4,7 @@ defmodule BoomerangFSM.PlayerTest do
   alias BoomerangFSM.Player
 
   @wait_to_catch_time 1000
+  @boomerang_flight_time 200
 
   test "start_link arguments work correctly" do
     assert {:ok, player1} = Player.start_link name: "Player 1"
@@ -37,15 +38,56 @@ defmodule BoomerangFSM.PlayerTest do
   test "players become ready to catch after one throws a boomerang" do
     {player1, player2} = create_two_players |> assign_players_to_each_other
     Player.give_boomerang player1
-    Player.throw_boomerang player1
+    Player.throw_boomerang player1, test: :boomerang_lost
 
     assert {:ready_to_catch, _} = Player.state player1
     assert {:ready_to_catch, _} = Player.state player2
   end
 
+  test "a player will give up on waiting to catch after a while" do
+    {player1, player2} = create_two_players |> assign_players_to_each_other
+    Player.give_boomerang player1
+    Player.throw_boomerang player1, test: :boomerang_lost
+
+    :timer.sleep 50
+    assert {:ready_to_catch, _} = Player.state player2
+    :timer.sleep @wait_to_catch_time + 200
+    assert {:ready, _} = Player.state player2
+  end
+
+  test "a player is ready to throw again after they throw the boomerang and it returns" do
+    {player1, player2} = create_two_players |> assign_players_to_each_other
+    Player.give_boomerang player1
+    Player.throw_boomerang player1, test: :boomerang_return
+
+    :timer.sleep @boomerang_flight_time + 200
+    assert {:ready_to_throw, _} = Player.state player1
+    :timer.sleep @wait_to_catch_time
+    assert {:ready, _} = Player.state player2
+  end
+
+  test "a player's opponent is ready to throw after they catch a thrown boomerang" do
+    {player1, player2} = create_two_players |> assign_players_to_each_other
+    Player.give_boomerang player1
+    Player.throw_boomerang player1, test: :boomerang_opponent
+
+    :timer.sleep @boomerang_flight_time + 200
+    assert {:ready_to_throw, _} = Player.state player2
+    :timer.sleep @wait_to_catch_time
+    assert {:ready, _} = Player.state player1
+  end
+
   defp create_two_players do
-    {:ok, player1} = Player.start_link name: "Player 1", wait_to_catch_time: @wait_to_catch_time
-    {:ok, player2} = Player.start_link name: "Player 2", wait_to_catch_time: @wait_to_catch_time
+    {:ok, player1} = Player.start_link [
+      name: "Player 1",
+      wait_to_catch_time: @wait_to_catch_time,
+      boomerang_flight_time: @boomerang_flight_time
+    ]
+    {:ok, player2} = Player.start_link [
+      name: "Player 2",
+      wait_to_catch_time: @wait_to_catch_time,
+      boomerang_flight_time: @boomerang_flight_time
+    ]
     {player1, player2}
   end
 
